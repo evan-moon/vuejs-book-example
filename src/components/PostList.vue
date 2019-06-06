16bd1e811e003af6bc5a0202d4e1316ee47d22b4
Evan Moon <bboydart91@gmail.com>
2019-06-06 15:30:28 +0900

    PostList 컴포넌트까지 완료

---
+++ src/components/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+  <table>
+    <colgroup>
+      <col style="width: 10%;"/>
+      <col style="width: 60%"/>
+      <col style="width: 10%"/>
+      <col style="width: 20%"/>
+    </colgroup>
+    <thead>
+      <tr>
+        <th scope="col">번호</th>
+        <th scope="col">제목</th>
+        <th scope="col">작성자</th>
+        <th scope="col">작성일</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="post in posts" :key="post.id">
+        <td scope="col">{{ post.id }}</td>
+        <td scope="col">
+          <!-- 제목을 클릭할 경우, 해당하는 게시물의 상세보기 페이지로 이동 되어야하나, 지금은 현재 상세보기 페이지가 없기 때문에 임의로 PostListPage로 지정한다. -->
+          <router-link :to="{ name: 'PostListPage' }">{{ post.title }}</router-link>
+          [{{ post.comments.length }}]
+        </td>
+        <td scope="col">{{ post.user.name }}</td>
+        <td scope="col">{{ post.createdAt }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PostList',
+  props: {
+    posts: {
+      type: Array
+    }
+  }
+}
+</script>
---
+++ src/pages/PostListPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="post-list-page">
+    <h1>포스트 게시글</h1>
+    <post-list :posts="posts" />
+  </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList'
+import api from '@/api'
+
+export default {
+  name: 'PostListPage',
+  components: { PostList },
+  data () {
+    return {
+      posts: []
+    }
+  },
+  created () {
+    api.get('/posts')
+      .then(res => {
+        this.posts = res.data
+      })
+  }
+}
+</script>
